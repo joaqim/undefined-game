2edd2731b29239d22e2f01587bf7210762436753
Joaqim Planstedt <gpg@joaqim.xyz>
2023-06-08 16:55:54 +0200

    Refactor: Minor syntax change

--- src/App.tsx
+++ src/App.tsx
@@ -22,59 +22,57 @@ function App() {
 
       https://github.com/hmans/composer-suite/tree/main/packages/render-composer
       */}
-      <RC.Canvas shadows dpr={1}>
-        <StrictMode>
-          <RC.RenderPipeline>
-            {/* Define an EffectPass with some postprocessing */}
-            <RC.EffectPass>
-              <RC.SMAAEffect />
-              {/* <RC.TiltShiftEffect focusArea={6} kernelSize={4} feather={10} /> */}
-              <RC.SelectiveBloomEffect intensity={5} />
-              <RC.VignetteEffect darkness={0.4} />
-            </RC.EffectPass>
+      <RC.Canvas strict shadows dpr={1}>
+        <RC.RenderPipeline>
+          {/* Define an EffectPass with some postprocessing */}
+          <RC.EffectPass>
+            <RC.SMAAEffect />
+            {/* <RC.TiltShiftEffect focusArea={6} kernelSize={4} feather={10} /> */}
+            <RC.SelectiveBloomEffect intensity={5} />
+            <RC.VignetteEffect darkness={0.4} />
+          </RC.EffectPass>
 
-            {/* Scene Background */}
-            <color args={["#223"]} attach="background" />
+          {/* Scene Background */}
+          <color args={["#223"]} attach="background" />
 
-            <Suspense>
-              {/* Environment */}
-              <Environment preset="sunset" />
+          <Suspense>
+            {/* Environment */}
+            <Environment preset="sunset" />
 
-              {/* Lights */}
-              <ambientLight intensity={0.2} />
-              <directionalLight position={[10, 10, 30]} intensity={1} />
+            {/* Lights */}
+            <ambientLight intensity={0.2} />
+            <directionalLight position={[10, 10, 30]} intensity={1} />
 
-              {/* The main camera. We can declare ECS entities ad-hoc like this: */}
-              <ECS.Entity>
-                <ECS.Component name="isCamera" data={true} />
+            {/* The main camera. We can declare ECS entities ad-hoc like this: */}
+            <ECS.Entity>
+              <ECS.Component name="isCamera" data={true} />
 
-                {/* If an ECS component has a React child, the ref to the rendered element
+              {/* If an ECS component has a React child, the ref to the rendered element
                 will be assigned to the specified ECS component: */}
-                <ECS.Component name="transform">
-                  {/* <PerspectiveCamera 
+              <ECS.Component name="transform">
+                {/* <PerspectiveCamera
                     position={[0, 0, 1000]}
                     zoom={undefined}
                   */}
-                  <OrthographicCamera
-                    position={[0, 0, 10]}
-                    zoom={20}
-                    makeDefault
-                    matrixWorldAutoUpdate={undefined}
-                    getObjectsByProperty={undefined}
-                  />
-                </ECS.Component>
-              </ECS.Entity>
+                <OrthographicCamera
+                  position={[0, 0, 10]}
+                  zoom={20}
+                  makeDefault
+                  matrixWorldAutoUpdate={undefined}
+                  getObjectsByProperty={undefined}
+                />
+              </ECS.Component>
+            </ECS.Entity>
 
-              {/* The actual game entities: */}
-              <Player />
-              <Asteroids />
-              <Bullets />
+            {/* The actual game entities: */}
+            <Player />
+            <Asteroids />
+            <Bullets />
 
-              {/* We've bundled all game systems in a top-level <Systems/> component. */}
-              <Systems />
-            </Suspense>
-          </RC.RenderPipeline>
-        </StrictMode>
+            {/* We've bundled all game systems in a top-level <Systems/> component. */}
+            <Systems />
+          </Suspense>
+        </RC.RenderPipeline>
       </RC.Canvas>
     </>
   );
